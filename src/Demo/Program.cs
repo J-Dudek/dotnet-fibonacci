2d5532568e53270e7a3efa8eff65b049ec2eb48f
Julien Dudek <julien.dudek@gmail.com>
2021-10-18 14:27:52 +0200

    Add dependencies for Log and object methode for properties

--- src/Demo/Program.cs
+++ src/Demo/Program.cs
@@ -1,7 +1,26 @@
 ﻿using System;
 using System.Diagnostics;
-
+using System.IO;
 using Fibonacci;
+using Microsoft.Extensions.Configuration;
+
+
+
+
+var environmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+IConfiguration configuration = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddEnvironmentVariables()
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .AddJsonFile($"appsettings.{environmentName}.json", optional: true, reloadOnChange: true)
+    .Build();
+
+
+var applicationSection = configuration.GetSection("Application");
+var applicationConfig = applicationSection.Get<ApplicationConfig>();
+
+Console.WriteLine($"Application Name : {applicationConfig.Name}");
+Console.WriteLine($"Application Message : {applicationConfig.Message}");
 
 Stopwatch stopwatch = new Stopwatch();
 stopwatch.Start();
@@ -13,3 +32,10 @@ foreach (var task in tasks) Console.WriteLine($"Fibo result : {task}");
 
 stopwatch.Stop();
 Console.WriteLine($"{stopwatch.Elapsed.Seconds}s");
+
+
+public class ApplicationConfig
+{
+    public string Name { get; set; }
+    public string Message { get; set; }
+}
